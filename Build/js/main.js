bb93f058003e5eddfc33528916b2e7c6f54c31a6
Mk <mabdi0635@gmail.com>
2020-02-23 22:49:31 +0300

    :)

--- Build/js/main.js
+++ Build/js/main.js
@@ -11,3 +11,37 @@ var swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
+// custom js
+const nav_display = document.getElementById('nav-display');
+const nav = document.querySelector('.nav-list'); 
+const bd = document.querySelector('body');
+const smallNav = document.getElementById('mobile-nav');
+// nav_display.addEventListener('click', () => {
+//   bd.style.opacity = '.3';
+//   nav.style.opacity = '1';
+//   nav.classList.toggle('display');
+// });
+ 
+
+let mobileNav = `
+<ul class="nav-mobile">
+<li>
+  <a href="index.html" class="nav-link">Home</a>
+</li>
+<li>
+  <a href="Contact.html" class="nav-link">Contact</a>
+</li>
+<li>
+  <a href="About.html" class="nav-link">About</a>
+</li>
+<li>
+  <a href="Location.html" class="nav-link">Location</a>
+</li> 
+</ul>
+`
+
+if(bd.clientWidth < 731){
+ smallNav.innerHTML = mobileNav;
+}  
